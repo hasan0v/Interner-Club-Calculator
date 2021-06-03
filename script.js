0c1a0ba0ea91e126cca087c9c1e3286c34467aae
AlienHM <68409324+AlienHM@users.noreply.github.com>
2021-06-04 01:30:06 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log(new Date().getHours(994))
+console.log(new Date().getMinutes(994))
+
+isaat = new Date().getHours(994)
+ideq = new Date().getMinutes(994)
+
+// saat = document.getElementById('saat').value()
+// deq = document.getElementById('deq').value()
+// console.log(saat, deq)
+
